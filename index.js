5c24937558d3b5b1d54fc0c5c94e91281cf54b6d
Stefan Inman <stefan.inman@gmail.com>
2022-01-03 15:12:47 -0600

    Completed Assignement

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Joey',
+    streetAddress:'101 Pokemon St'
+}
+
+
+
+function updateEmployeeWithKeyAndValue(yo, what, up) {
+    return {
+        ...yo,
+        [what]: up,
+    };    
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(testy, test, tester) {
+    testy[test] = tester;
+    return testy;
+}
+
+function deleteFromEmployeeByKey(peace, out) {
+    const newObj = {...peace};
+    delete newObj[out];
+    return newObj; 
+}
+
+function destructivelyDeleteFromEmployeeByKey(he, gone) {
+    delete he[gone];
+    return he;
+}
